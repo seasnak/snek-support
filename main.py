3ae456228b8cf7cfe96d51540111105b7f927836
Seasnak <sasank1020@gmail.com>
2020-12-17 20:26:52 -0800

    Bandaid Update

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import discord
+from discord.ext import commands
+import os
 import random
-import base64
+
+#LOCAL FILE IMPORTS
+from cogs import *
+import config
+import hello
+import anime
 
 secret_dict = {}
 
-client = discord.Client()
-
+bot = commands.Bot(command_prefix='!')
 
 def populate_dict(dict, text):
     elements = open(text, 'r').read().split() # getting SECRETs
@@ -14,23 +20,67 @@ def populate_dict(dict, text):
         dict.update({value[0]: value[1]})
     return dict
 
-@client.event
-async def on_message(message):
-    message_string = message.content.lower()
-    command_list = message_string.split()
-    command = message_string[0]
-    print(message_string) #DEBUG
+# @bot.event
+# async def on_message(message):
+#     if message.author == bot.user: return #message was sent by self
+#     return
 
-    if message.author == client.user: return
-    
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
     return
 
-@client.event
-async def on_ready():
-    print(f"Logged in as {client.user}".format(client))
+@bot.command()
+async def load(ctx, ext):
+    for extension in config.extensions:
+        if extension == ext:
+            print(f"Extension {ext} already exists!")
+            return
+
+    try:
+        bot.load_extension(ext)
+        print(f"Loading extension {ext}")
+        config.extension.append(ext)
+    except Exception as e:
+        print(f"Failed to load extension {ext}\nError: {type(e).__name__}: {e}")
+    return
+
+@bot.command()
+async def unload(ctx, ext):
+    if ext in config.extensions:
+        print(f"Extension {ext} either does not exist, or is not currently loaded")
+        return
+
+    try:
+        bot.unload_extension(ext)
+        print(f"Unloading extension {ext}")
+        config.extensions.remove(ext)
+    except Exception as e:
+        print(f"Failed to unload extension {ext}\nError: {type(e).__name__}: {e}")
+    return
+
+@bot.command()
+async def reload(ctx, ext):
+    if ext in config.extensions:
+        print(f"Extension {ext} either does not exist, or is not currently loaded")
+        return
+
+    try:
+        bot.reload_extension(ext)
+        print(f"Reloading extension {ext}")
+    except Exception as e:
+        print(f"Failed to reload extension {ext}\nError: {type(e).__name__}: {e}")
     return
 
 if __name__ == "__main__":
     populate_dict(secret_dict, "secret.txt")
 
-    client.run(secret_dict['token']) #run bot
+    # print(config.extensions)
+    for ext in config.extensions:
+        try:
+            bot.load_extension(ext)
+            print(f"Loading extension {ext}")
+        except Exception as e:
+            print(f"Failed to laod extension {ext}\nError: {type(e).__name__}: {e}")
+
+    bot.run(secret_dict['token']) #run bot
