d64646fcb42ac432d8e387e3ae3fd07d755e3141
Seasnak <sasank1020@gmail.com>
2020-12-21 01:42:43 -0800

    updated anime and 8ball commands

--- cogs/8ball.py
+++ cogs/8ball.py
@@ -14,21 +14,29 @@ class EightBall(commands.Cog):
 
     @commands.command()
     async def eightBall(self, ctx):
+        """ask me a question (usage: ask a yes/no question no ! command necessary)"""
         rand = random.randint(0, len(responses)-1)
         await ctx.channel.send(responses[rand])
         return
 
     @commands.Cog.listener()
     async def on_message(self, ctx):
+        """ask me a question!"""
         if ctx.author == self.bot.user: return #don't reply to self
-        question = ctx.content.lower()
-        if question.startswith(('is', 'are', 'do', 'should', 'would', 'could', 'may', 'am', 'will', 'can')):
+        question = ""
+        try:
+            question = ctx.content.lower().split()[0]
+        except Exception as e:
+            print(f"{type(e).__name__}: {e}")
+        if question in ['is', 'are', 'do', 'should', 'would', 'could', 'may', 'am', 'will', 'can', 'have', 'does', 'was', 'did']:
             rand = random.randint(0, len(responses)-1)
             await ctx.channel.send(responses[rand])
         return
 
     @commands.command()
-    async def responses(self, ctx):
+    async def responses(self, ctx, option = ""):
+        """list all responses (usage: !responses)"""
+        # if options.startswith('a'): #add response
         message = ""
         for i in range(len(responses)):
             message += f"{i}. {responses[i]}\n"
@@ -37,13 +45,15 @@ class EightBall(commands.Cog):
 
     @commands.command()
     async def response_add(self, ctx, response):
+        """add a new response (usage: !response_add <new response>)"""
         responses.append(response)
         print(f"Added new response to 8ball: {response}")
         return
 
     @commands.command(alias = "response_remove")
     async def response_rm(self, ctx, num: int):
-        print(f"Removed reponse: {responses.pop(num)}")
+        """remove an existing response (usage: !respone_rm <reponse number>)"""
+        print(f"Removed reponse: {responses.pop(num-1)}")
         return
 
 
--- cogs/anime.py
+++ cogs/anime.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import discord
 import random
 
 class Anime(commands.Cog):
@@ -8,7 +9,39 @@ class Anime(commands.Cog):
 
     @commands.command()
     async def anime(self, ctx):
-        print("Anime command called")
+        """Anime Search Command"""
+        rand = random.randint(0, 99)
+        if rand == 1:
+            embed = discord.Embed(
+                title = "Cory in the House",
+                url = "https://www.youtube.com/watch?app=desktop&v=KumtJtNqiT0",
+                color = ctx.author.color
+            )
+            embed.set_thumbnail(url="https://m.media-amazon.com/images/M/MV5BMTUzOTU1MjY3M15BMl5BanBnXkFtZTcwMTUyMTk0MQ@@._V1_UY268_CR4,0,182,268_AL_.jpg")
+            embed.add_field(name="Genre", value="Comedy, Family", inline=True)
+            embed.add_field(name="Year", value="2007-2008", inline=True)
+            embed.set_footer(text="anime search result")
+            await ctx.channel.send(embed=embed)
+        else:
+            await ctx.channel.send("weeb.")
+        return
+
+    async def cory(self, ctx):
+        """Cory in the House Roulette"""
+        rand = random.randint(0, 99)
+        if rand == 1:
+            embed = discord.Embed(
+                title = "Cory in the House",
+                url = "https://www.youtube.com/watch?app=desktop&v=KumtJtNqiT0",
+                color = ctx.author.color
+            )
+            embed.set_thumbnail(url="https://m.media-amazon.com/images/M/MV5BMTUzOTU1MjY3M15BMl5BanBnXkFtZTcwMTUyMTk0MQ@@._V1_UY268_CR4,0,182,268_AL_.jpg")
+            embed.add_field(name="Genre", value="Comedy, Family", inline=True)
+            embed.add_field(name="Year", value="2007-2008", inline=True)
+            embed.set_footer(text="anime search result")
+            await ctx.channel.send(embed=embed)
+        else:
+            await ctx.channel.send("weeb.")
         return
 
 def setup(bot):
