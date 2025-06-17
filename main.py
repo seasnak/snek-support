3474d67f391d438d3709020864357deb86aca615
Sasank Madineni <sasank1020@gmail.com>
2025-06-16 20:29:19 -0700

    restructuring code

--- main.py
+++ main.py
@@ -13,6 +13,33 @@ from cogs import *
 
 bot = commands.Bot(command_prefix='!')
 
+class SnekSupportBot:
+    def __init__(self, command_prefix, intents): 
+        intents = discord.Intents.default()
+        intents.message_content = True
+        super().__init__(command_prefix=command_prefix, intents=intents)
+        return
+
+    async def setup_hook(self):
+        # load extensions 
+        errored_extensions = []
+        for extension in config.extensions:
+            try:
+                await bot.load_extension(extension)
+                print(f"Loaded extensions \'{extension}\'")
+            except Exception as e:
+                print(f"Failed to load extensions \'{extension}\' \n{type(e).__name__}: {e}")
+                errored_extensions.append(extension)
+            
+            print(f"Loaded extensions: {[x for x in bot.cogs]}")
+        
+        for extension in errored_extensions: 
+            config.extensions.remove(extension)
+            
+        return
+
+        return
+
 # @bot.event
 # async def on_message(message):
 #     if message.author == bot.user: return #message was sent by self
@@ -81,19 +108,19 @@ async def update(ctx):
             print(f"Failed to reload extension {extension}\n{type(e).__name__}: {e}")
     return
 
-@bot.command()
-async def sys(ctx, *, message):
-    print(f"bash: {message}")
-    if f"{ctx.author.id}" not in config.admins: return
-    try:
-        output = subprocess.check_output(message)
-        await ctx.channel.send(output.decode("utf-8"))
-    except Exception as e:
-        print(f"{type(e).__name__} : {e}")
-    return
+# @bot.command()
+# async def sys(ctx, *, message):
+#     print(f"bash: {message}")
+#     if f"{ctx.author.id}" not in config.admins: return
+#     try:
+#         output = subprocess.check_output(message)
+#         await ctx.channel.send(output.decode("utf-8"))
+#     except Exception as e:
+#         print(f"{type(e).__name__} : {e}")
+#     return
 
 @bot.command()
-async def extensions(ctx):
+async def getloaded(ctx):
     message = "Currently Loaded Extensions:\n"
     i = 1
     for extension in config.extensions:
@@ -102,14 +129,14 @@ async def extensions(ctx):
     await ctx.channel.send(message)
     return
 
+bot.run(config.token) #run bot
 
-if __name__ == "__main__":
-    # print(config.extensions)
-    for ext in config.extensions:
-        try:
-            bot.load_extension(ext)
-            print(f"Loading extension {ext}")
-        except Exception as e:
-            print(f"Failed to load extension {ext}\n{type(e).__name__}: {e}")
-
-    bot.run(config.token) #run bot
+# if __name__ == "__main__":
+#     # print(config.extensions)
+#     for ext in config.extensions:
+#         try:
+#             bot.load_extension(ext)
+#             print(f"Loading extension {ext}")
+#         except Exception as e:
+#             print(f"Failed to load extension {ext}\n{type(e).__name__}: {e}")
+#
