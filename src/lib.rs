f940f4f22fb0743ec349d82a05a53f56c567549c
Sasank Madineni <sasank1020@gmail.com>
2025-06-22 00:19:44 -0700

    minor changes

--- src/lib.rs
+++ src/lib.rs
@@ -27,23 +27,27 @@ struct Item {
 struct User {
     #[pyo3(get, set)]
     pub id: i64,
+    #[pyo3(get, set)]
     pub name: String,
+    #[pyo3(get, set)]
     pub social_credit: i16,
-
-    #[pyo3(get)]
     pub items: HashMap<String, Item>,
 }
 
 #[pymethods]
 impl User {
     #[new]
-    fn new(id: i64, name: String, social_credit: Option<i16>) -> Self {
+    fn new(id: i64, name: String, social_credit: i16) -> Self {
         User {
             id,
             name,
-            social_credit.unwrap_or(1000),
-            {},
+            social_credit,
+            items: HashMap::new(),
         }
     }
-}
 
+    pub fn add_item(&mut self, name: String, item: &Item) {
+        if self.items.contains_key(&name) {}
+        self.items.insert(name, *item);
+    }
+}
