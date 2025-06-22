34b21408890f5f9aaa1cfe6f639b1fe2f2e82fd1
Sasank Madineni <sasank1020@gmail.com>
2025-06-22 15:19:09 -0700

    started working on clone/copy functions for Item objects

--- src/lib.rs
+++ src/lib.rs
@@ -23,6 +23,31 @@ struct Item {
     pub quantity: i8,
 }
 
+#[pymethods]
+impl Item {
+    #[new]
+    fn new(item_name: String, item_quantity: Option<i8>) -> Self {
+        let num_items = match item_quantity {
+            Some(n) => n,
+            None => 1,
+        };
+
+        Item {
+            name: item_name, // might need to do a copy?
+            quantity: num_items,
+        }
+    }
+}
+
+impl Copy for Item {
+    fn copy(&self) -> Self {
+        Item {
+            name: self.name.clone(),
+            quantity: self.quantity,
+        }
+    }
+}
+
 #[pyclass]
 struct User {
     #[pyo3(get, set)]
