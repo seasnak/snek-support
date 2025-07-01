3309d64da6e30b93c58d9cd9793d6bea1e4592c3
Sasank Madineni <sasank1020@gmail.com>
2025-06-30 17:15:37 -0700

    flipped sign

--- cogs/social_credit.py
+++ cogs/social_credit.py
@@ -88,9 +88,9 @@ class SocialCredit(commands.Cog):
         if "random" in target.lower():
             members = [member.id for member in context.guild.members]
             random_target = members[random.randint(0, len(members))]
-            await self.adjust_id_credit(context, random_target, -amount)
+            await self.adjust_id_credit(context, random_target, amount)
         else:
-            await self.adjust_credit(context, target, -amount)
+            await self.adjust_credit(context, target, amount)
         return
 
     @commands.hybrid_command(
