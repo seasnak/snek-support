f078ee94b0774be3e14258f5f761632d9290da42
Sasank Madineni <sasank1020@gmail.com>
2025-06-30 21:44:31 -0700

    started working on snailmail

--- cogs/snailmail.py
+++ cogs/snailmail.py
@@ -1,1 +1,36 @@
+from discord import NotFound
+from discord.ext import commands
 
+from cogs.social_credit import SocialCredit
+import config
+import random
+import utils
+import pickle
+from enum import Enum
+
+class SnailMailMethod(Enum):
+    SLUGGISH=1,
+    SLOW=2,
+
+
+class SnailMail(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot 
+
+    @commands.hybrid_command(
+        name="snailmail",
+        description="send a message to someone!"
+    )
+    async def snailmail(self, context: commands.Context, method: SnailMailMethod, recipient: str, message: str):
+        
+        
+        return
+
+
+async def setup(bot):
+    await bot.add_cog(SocialCredit(bot))
+    return
+
+async def teardown(bot):
+    await bot.remove_cog(SocialCredit(bot))
+    return
